2d1e70ecf2944ff53aabb6f5e8d086d9e5f8a77a
saneftw <39649316+saneftw@users.noreply.github.com>
2022-03-13 22:42:29 +0200

    Update config.h

--- config.h
+++ config.h
@@ -113,29 +113,31 @@ float alphaUnfocus;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	"#cc241d",
-	"#98971a",
-	"#d79921",
-	"#458588",
-	"#b16286",
-	"#689d6a",
-	"#a89984",
-	"#928374",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
-	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
-	[255] = 0,
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#add8e6", /* 256 -> cursor */
-	"#555555", /* 257 -> rev cursor*/
-	"#282828", /* 258 -> bg */
-	"#ebdbb2", /* 259 -> fg */
+"#ffffff",
+ "#ffffff",
+ "#517F8D",
+ "#6C8B91",
+ "#869696",
+ "#a1a19a",
+ "#e6e6dc",
+ "#fafaf8",
+ "#ff5a67",
+ "#f08e48",
+ "#ffcc1b",
+ "#7fc06e",
+ "#5dd7b9",
+ "#14747e",
+ "#9a70a4",
+ "#c43060",
+
+[255] = 0,
+/* more colors can be added after 255 to use with DefaultXX */
+"#add8e6", /* 256 -> cursor */
+"#555555", /* 257 -> rev cursor*/
+"#000000", /* 258 -> bg */
+"#ffffff", /* 259 -> fg */
 };
+
 
 
 /*
